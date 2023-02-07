2b2d8e536cf541225bdd8504fdf4d76ae8754bff
Ryxray <grdeshayes@laposte.net>
2023-02-07 16:25:49 +0100

    Centrer la div

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        "bg-button-color": "var(--bg-button-color)",
-      },
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "bg-button-color": "var(--bg-button-color)",
+            },
+        },
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
