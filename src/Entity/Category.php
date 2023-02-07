a97d93614cee2f60cfe5b0b28c27b2f0e2de8bd7
Idriss <idriss24@hotmail.fr>
2023-02-07 12:18:18 +0100

    BACK_feat(entity)
    creation entité: Category/Product + relation

---
+++ src/Entity/Category.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $label = null;
+
+    #[ORM\ManyToMany(targetEntity: product::class, inversedBy: 'Categorys')]
+    private Collection $product_id;
+
+    public function __construct()
+    {
+        $this->product_id = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, product>
+     */
+    public function getProductId(): Collection
+    {
+        return $this->product_id;
+    }
+
+    public function addProductId(product $productId): self
+    {
+        if (!$this->product_id->contains($productId)) {
+            $this->product_id->add($productId);
+        }
+
+        return $this;
+    }
+
+    public function removeProductId(product $productId): self
+    {
+        $this->product_id->removeElement($productId);
+
+        return $this;
+    }
+}
