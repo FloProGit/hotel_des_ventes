560f12a059925b70e0396b69885912f575ee1478
Idriss <idriss24@hotmail.fr>
2023-02-08 10:54:41 +0100

    BACK_FEAT(product fixture)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,17 +1,26 @@
 <?php
 
 namespace App\DataFixtures;
-
+use App\Entity\Product;
+use Faker;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create();
+        for($i = 0 ; $i < 100; $i++){
+            $product = new Product();
+            $product->setName($faker->word())
+                    ->setDescription($faker->sentence())
+                    ->setPriceExclTaxe($faker->randomNumber(2, false))
+                    ->setBarCode($faker->ean8());
+            $manager->persist($product);
+            $manager->flush();
+        }
 
-        $manager->flush();
     }
 }
