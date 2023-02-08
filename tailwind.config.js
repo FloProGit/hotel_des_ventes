fb89d735d86033ea96b858a4c38b535c816ad606
Ryxray <grdeshayes@laposte.net>
2023-02-08 11:07:13 +0100

    Add style header + img

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        "primary-color": "var(--primary-color)",
-        "secondary-color": "var(--secondary-color)",
-        "text-color": "var(--text-color)"
-      },
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "primary-color": "var(--primary-color)",
+                "secondary-color": "var(--secondary-color)",
+                "text-color": "var(--text-color)"
+            },
+            container: {
+                padding: {
+                    DEFAULT: '1rem',
+                    sm: '2rem',
+                    lg: '4rem',
+                    xl: '5rem',
+                    '2xl': '6rem',
+                },
+                
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
