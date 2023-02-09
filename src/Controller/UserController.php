5cf7cbe14bd5397c5c7747183b87d17ee0732380
florian <sudreflorianpro@gmail.com>
2023-02-09 15:49:53 +0100

    BACK_feature(add remove cart): add user controller add and remove in cart

---
+++ src/Controller/UserController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserController extends AbstractController
+{
+
+    public function addProductToCart(Request $request,ManagerRegistry  $doctrine)
+    {
+        if(!$this->getUser())
+        {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $productId = str_replace('/products/add_to_cart/', '', $request->getPathInfo());
+
+         $user = $this->getUser();
+        $currentCart = $user->getCart();
+
+        $entityManager = $doctrine->getManager();
+        $userRep = $entityManager->getRepository(User::class)->find(1);
+
+         if(array_key_exists($productId,$currentCart))
+         {
+             $currentCart[$productId] = (intval($currentCart[$productId]))+1;
+         }
+         else
+         {
+             $currentCart[$productId] = 1;
+         }
+
+        $userRep->setCart($currentCart);
+
+        $entityManager->flush();
+
+        dd($this->getUser());
+
+    }
+
+    public function removeProductToCart(Request $request,ManagerRegistry  $doctrine)
+    {
+        if(!$this->getUser())
+        {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $productId = str_replace('/products/remove_to_cart/', '', $request->getPathInfo());
+
+        $user = $this->getUser();
+        $currentCart = $user->getCart();
+
+        $entityManager = $doctrine->getManager();
+        $userRep = $entityManager->getRepository(User::class)->find(1);
+
+        if(array_key_exists($productId,$currentCart))
+        {
+
+            $currentCart = array_filter($currentCart, fn($key) => !in_array($key, [$productId]), ARRAY_FILTER_USE_KEY);
+
+        }
+
+        $userRep->setCart($currentCart);
+
+        $entityManager->flush();
+
+    }
+}
