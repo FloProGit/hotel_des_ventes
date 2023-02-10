fd969072078a33a9723e14fb6debc228ba567dc6
Baptiste Martin <81614331+baptiste-martin-campus@users.noreply.github.com>
2023-02-10 14:29:05 +0100

    [Feature][Home] change homepage

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
-
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomePageController extends AbstractController
 {
-    public function show():Response
+    public function show(ProductRepository $repository): Response
     {
-        return $this->render('base.html.twig');
+        $products = $repository->findAll();
+        $products = array_slice($products, 0, 10);
+        return $this->render('home.html.twig', ['products' => $products]);
     }
-
 }
