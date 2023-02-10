15d32b3e707ea4d0462dafed7c5e3cd910ad8a2a
florian <sudreflorianpro@gmail.com>
2023-02-10 15:21:33 +0100

    FRONT_feat(cart): add cart

--- assets/app.js
+++ assets/app.js
@@ -14,9 +14,6 @@ import 'tw-elements';
 
 
 
-function toggleSlideover(){
-    document.getElementById('slideover-container').classList.toggle('invisible');
-    document.getElementById('slideover-bg').classList.toggle('opacity-0');
-    document.getElementById('slideover-bg').classList.toggle('opacity-50');
-    document.getElementById('slideover').classList.toggle('translate-x-full');
-}
+
+
+console.log('hello');
